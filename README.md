4e0a8100228ba1c17d69dc812c5b1352b3a4db80
Lukas Kropatschek <lukas@kropatschek.net>
2014-03-03 20:10:12 +0100

    readme, localized and added mags to crates

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 lkr_ammo
 ==============
+This addon has two goals. The first is to bring the ballistics of several rounds to a realistic level. I'm
+using Spartan0536's data provided in his thread in the BI Forums [1], to create rounds and magazines. Big thanks to him at this point!
+
+The second goal is to unify/centralize rounds and magazines. As of now there are several great weapon addons which are all using different types of
+rounds and magazines, mostly created by the addon itself. To reduce redundancy with the most common rounds and magazines this addon provides a solid base
+and I encourage every addon creator to use the magazines provided by the addon.
+
+To get you started I'll be adding several optional configs for some common weapon addons.
+
+We start with 5.56x45 and as soon as Spartan0536 provides more ballistic configs I'll add more rounds/magazines.
+
+[1] http://forums.bistudio.com/showthread.php?173372-ArmA-III-5-56x45mm-Realistic-Ballistics
+
+lkr_556x45_nato.pbo
+--------------
+This addon adds two new rounds and updates the ballistics of the default 5.56x45 round.
+In the words of Spartan0536:
+
+__MK262 Mod 1__
+VERY lethal against unarmored targets, kills in 1 shot to the chest easily out to 400m, armor penetration on level III with SAAPI plates is about 5 shots at 350m, default 5.56 was killing in 7-8 at the same range
+
+__Mk 318 Mod 0__
+Very lethal against unarmoed targets, kills in 1 shot to the chest out to 400m, armor penetration on level III with SAAPI plates is about 5 shots @ 350m, very similar performance at medium range when comparing to Mk262 Mod 1, like its real life counterpart this round is most effective at ranges from 0-300m and is designed for enhanced performance out of shorter barrel rifles.
+
+__M855A1 EPR__
+Very lethal against unarmored targets, kills in 1 shot to the chest out to 350m-400m with some variation, armor penetration on level III with SAAPI plates is about 4 shots @ 350m, this round is a general purpose round like its real life counterpart, it has the best penetration rate of all 3 rounds but it is yaw dependent on damage so it also has the lowest hit factor to compensate for this as best as possible.
+
+### Classnames:
+
+New rounds:
+````
+B_556x45_Mk318_Ball
+B_556x45_Mk318_Ball_Tracer_Green
+B_556x45_Mk318_Ball_Tracer_Red
+B_556x45_Mk318_Ball_Tracer_Yellow
+B_556x45_Mk262_Ball
+B_556x45_Mk262_Ball_Tracer_Green
+B_556x45_Mk262_Ball_Tracer_Red
+B_556x45_Mk262_Ball_Tracer_Yellow
+````
+New magazines:
+````
+30Rnd_556x45_Mk318_Stanag
+30Rnd_556x45_Mk318_Stanag_Tracer_Green
+30Rnd_556x45_Mk318_Stanag_Tracer_Red
+30Rnd_556x45_Mk318_Stanag_Tracer_Yellow
+30Rnd_556x45_Mk262_Stanag
+30Rnd_556x45_Mk262_Stanag_Tracer_Green
+30Rnd_556x45_Mk262_Stanag_Tracer_Red
+30Rnd_556x45_Mk262_Stanag_Tracer_Yellow
+````
--- src/lkr_556x45_NATO/config.cpp
+++ src/lkr_556x45_NATO/config.cpp
@@ -94,57 +94,57 @@ class CfgMagazines {
 	class 30Rnd_556x45_Stanag : CA_Magazine
 	{
 		initSpeed = 960.12;
-		displayName = "5.56mm 30rnd M855A1";
-		descriptionShort = "Caliber: 5.56x45 mm STANAG<br />Rounds: 30<br />Type: M855A1 EPR";
+		displayName = $STR_30RND_556x45_DISPLAY_NAME;
+		descriptionShort = $STR_30RND_556x45_DESCRIPTION_SHORT;
 	};
 	
 	class 30Rnd_556x45_Stanag_Tracer_Green : 30Rnd_556x45_Stanag
 	{
-		displayName = "5.56mm 30rnd M855A1 Tracer Green";
-		descriptionShort = "Caliber: 5.56x45 mm STANAG Tracers - Green<br />Rounds: 30<br />Type: M855A1 EPR";
+		displayName = $STR_30RND_556x45_GREEN_TRACER_DISPLAY_NAME;
+		descriptionShort = $STR_30RND_556x45_GREEN_TRACER_DESCRIPTION_SHORT;
 	};
 	
 	class 30Rnd_556x45_Stanag_Tracer_Red : 30Rnd_556x45_Stanag
 	{
-		displayName = "5.56mm 30rnd M855A1 Tracer Red";
-		descriptionShort = "Caliber: 5.56x45 mm STANAG Tracers - Red<br />Rounds: 30<br />Type: M855A1 EPR";
+		displayName = $STR_30RND_556x45_RED_TRACER_DISPLAY_NAME;
+		descriptionShort = $STR_30RND_556x45_RED_TRACER_DESCRIPTION_SHORT;
 	};
 	
 	class 30Rnd_556x45_Stanag_Tracer_Yellow : 30Rnd_556x45_Stanag
 	{
-		displayName = "5.56mm 30rnd M855A1 Tracer Yellow";
-		descriptionShort = "Caliber: 5.56x45 mm STANAG Tracers - Yellow<br />Rounds: 30<br />Type: M855A1 EPR";
+		displayName = $STR_30RND_556x45_YELLOW_TRACER_DISPLAY_NAME;
+		descriptionShort = $STR_30RND_556x45_YELLOW_TRACER_DESCRIPTION_SHORT;
 	};
 	
 	class 30Rnd_556x45_Mk318_Stanag : 30Rnd_556x45_Stanag
 	{
 		ammo = "B_556x45_Mk318_Ball";
 		initSpeed = 950.3664;
-		displayName = "5.56mm 30rnd Mk318";
-		descriptionShort = "Caliber: 5.56x45 mm STANAG<br />Rounds: 30<br />Type: Mk318 Mod 0 SOST";
+		displayName = $STR_30RND_556x45_MK318_DISPLAY_NAME;
+		descriptionShort = $STR_30RND_556x45_MK318_DESCRIPTION_SHORT;
 	};
 	
 	class 30Rnd_556x45_Mk318_Stanag_Tracer_Green : 30Rnd_556x45_Mk318_Stanag
 	{
 		ammo = "B_556x45_Mk318_Ball_Tracer_Green";
-		displayName = "5.56mm 30rnd Mk318 Tracer Green";
-		descriptionShort = "Caliber: 5.56x45 mm STANAG Tracers - Green<br />Rounds: 30<br />Type: Mk318 Mod 0 SOST";
+		displayName = $STR_30RND_556x45_MK318_GREEN_TRACER_DISPLAY_NAME;
+		descriptionShort = $STR_30RND_556x45_MK318_GREEN_TRACER_DESCRIPTION_SHORT;
 		tracersEvery = 1;
 	};
 	
 	class 30Rnd_556x45_Mk318_Stanag_Tracer_Red : 30Rnd_556x45_Mk318_Stanag
 	{
 		ammo = "B_556x45_Mk318_Ball_Tracer_Red";
-		displayName = "5.56mm 30rnd Mk318 Tracer Red";
-		descriptionShort = "Caliber: 5.56x45 mm STANAG Tracers - Red<br />Rounds: 30<br />Type: Mk318 Mod 0 SOST";
+		displayName = $STR_30RND_556x45_MK318_RED_TRACER_DISPLAY_NAME;
+		descriptionShort = $STR_30RND_556x45_MK318_RED_TRACER_DESCRIPTION_SHORT;
 		tracersEvery = 1;
 	};
 	
 	class 30Rnd_556x45_Mk318_Stanag_Tracer_Yellow : 30Rnd_556x45_Mk318_Stanag
 	{
 		ammo = "B_556x45_Mk318_Ball_Tracer_Yellow";
-		displayName = "5.56mm 30rnd Mk318 Tracer Yellow";
-		descriptionShort = "Caliber: 5.56x45 mm STANAG Tracers - Yellow<br />Rounds: 30<br />Type: Mk318 Mod 0 SOST";
+		displayName = $STR_30RND_556x45_MK318_YELLOW_TRACER_DISPLAY_NAME;
+		descriptionShort = $STR_30RND_556x45_MK318_YELLOW_TRACER_DESCRIPTION_SHORT;
 		tracersEvery = 1;
 	};
 	
@@ -152,31 +152,31 @@ class CfgMagazines {
 	{
 		ammo = "B_556x45_Mk262_Ball";
 		initSpeed = 838.2;
-		displayName = "5.56mm 30rnd Mk262";
-		descriptionShort = "Caliber: 5.56x45 mm STANAG<br />Rounds: 30<br />Type: Mk262 Mod 1 SPR";
+		displayName = $STR_30RND_556x45_MK262_DISPLAY_NAME;
+		descriptionShort = $STR_30RND_556x45_MK262_DESCRIPTION_SHORT;
 	};
 	
 	class 30Rnd_556x45_Mk262_Stanag_Tracer_Green : 30Rnd_556x45_Mk318_Stanag
 	{
 		ammo = "B_556x45_Mk262_Ball_Tracer_Green";
-		displayName = "5.56mm 30rnd Mk262 Tracer Green";
-		descriptionShort = "Caliber: 5.56x45 mm STANAG Tracers - Green<br />Rounds: 30<br />Type: Mk262 Mod 1 SPR";
+		displayName = $STR_30RND_556x45_MK262_GREEN_TRACER_DISPLAY_NAME;
+		descriptionShort = $STR_30RND_556x45_MK262_GREEN_TRACER_DESCRIPTION_SHORT;
 		tracersEvery = 1;
 	};
 	
 	class 30Rnd_556x45_Mk262_Stanag_Tracer_Red : 30Rnd_556x45_Mk318_Stanag
 	{
 		ammo = "B_556x45_Mk262_Ball_Tracer_Red";
-		displayName = "5.56mm 30rnd Mk262 Tracer Red";
-		descriptionShort = "Caliber: 5.56x45 mm STANAG Tracers - Red<br />Rounds: 30<br />Type: Mk262 Mod 1 SPR";
+		displayName = $STR_30RND_556x45_MK262_RED_TRACER_DISPLAY_NAME;
+		descriptionShort = $STR_30RND_556x45_MK262_RED_TRACER_DESCRIPTION_SHORT;
 		tracersEvery = 1;
 	};
 	
 	class 30Rnd_556x45_Mk262_Stanag_Tracer_Yellow : 30Rnd_556x45_Mk318_Stanag
 	{
 		ammo = "B_556x45_Mk262_Ball_Tracer_Yellow";
-		displayName = "5.56mm 30rnd Mk262 Tracer Yellow";
-		descriptionShort = "Caliber: 5.56x45 mm STANAG Tracers - Yellow<br />Rounds: 30<br />Type: Mk262 Mod 1 SPR";
+		displayName = $STR_30RND_556x45_MK262_YELLOW_TRACER_DISPLAY_NAME;
+		descriptionShort = $STR_30RND_556x45_MK262_YELLOW_TRACER_DESCRIPTION_SHORT;
 		tracersEvery = 1;
 	};
 };
@@ -235,6 +235,113 @@ class CfgWeapons {
 			"30Rnd_556x45_Mk262_Stanag_Tracer_Red",
 			"30Rnd_556x45_Mk262_Stanag_Tracer_Green",
 			"30Rnd_556x45_Mk262_Stanag_Tracer_Yellow"
+		};
+	};
+};
+
+class CfgVehicles {
+	class ReammoBox_F;
+	class IND_Box_Base : ReammoBox_F {
+		class TransportMagazines;
+	};
+	class NATO_Box_Base : ReammoBox_F {
+		class TransportMagazines;
+	};
+	class EAST_Box_Base : ReammoBox_F {
+		class TransportMagazines;
+	};
+	class B_supplyCrate_F : ReammoBox_F {
+		class TransportMagazines;
+	};
+	
+	class Box_IND_Ammo_F: IND_Box_Base {
+		class TransportMagazines : TransportMagazines {
+			class _xx_30_Rnd_556x45_Mk318_Stanag {
+				count = 12;
+				magazine  = "30Rnd_556x45_Mk318_Stanag";
+			};
+			class _xx_30_Rnd_556x45_Mk262_Stanag {
+				count = 12;
+				magazine  = "30Rnd_556x45_Mk262_Stanag";
+			};
+		};
+	};
+	
+	class Box_IND_Wps_F: IND_Box_Base {
+		class TransportMagazines : TransportMagazines {
+			class _xx_30_Rnd_556x45_Mk318_Stanag {
+				count = 8;
+				magazine  = "30Rnd_556x45_Mk318_Stanag";
+			};
+			class _xx_30_Rnd_556x45_Mk262_Stanag {
+				count = 8;
+				magazine  = "30Rnd_556x45_Mk262_Stanag";
+			};
+		};
+	};
+	
+	class Box_IND_WpsSpecial_F: IND_Box_Base {
+		class TransportMagazines : TransportMagazines {
+			class _xx_30_Rnd_556x45_Mk318_Stanag {
+				count = 3;
+				magazine  = "30Rnd_556x45_Mk318_Stanag";
+			};
+			class _xx_30_Rnd_556x45_Mk262_Stanag {
+				count = 3;
+				magazine  = "30Rnd_556x45_Mk262_Stanag";
+			};
+		};
+	};
+	
+	class Box_NATO_WpsSpecial_F: NATO_Box_Base {
+		class TransportMagazines : TransportMagazines {
+			class _xx_30_Rnd_556x45_Mk318_Stanag {
+				count = 3;
+				magazine  = "30Rnd_556x45_Mk318_Stanag";
+			};
+			class _xx_30_Rnd_556x45_Mk262_Stanag {
+				count = 3;
+				magazine  = "30Rnd_556x45_Mk262_Stanag";
+			};
+		};
+	};
+	
+	class Box_East_WpsSpecial_F: EAST_Box_Base {
+		class TransportMagazines : TransportMagazines {
+			class _xx_30_Rnd_556x45_Mk318_Stanag {
+				count = 3;
+				magazine  = "30Rnd_556x45_Mk318_Stanag";
+			};
+			class _xx_30_Rnd_556x45_Mk262_Stanag {
+				count = 3;
+				magazine  = "30Rnd_556x45_Mk262_Stanag";
+			};
+		};
+	};
+	
+	class I_supplyCrate_F: B_supplyCrate_F {
+		class TransportMagazines : TransportMagazines {
+			class _xx_30_Rnd_556x45_Mk318_Stanag {
+				count = 24;
+				magazine  = "30Rnd_556x45_Mk318_Stanag";
+			};
+			class _xx_30_Rnd_556x45_Mk262_Stanag {
+				count = 24;
+				magazine  = "30Rnd_556x45_Mk262_Stanag";
+			};
+		};
+	};
+	
+	class IG_supplyCrate_F: B_supplyCrate_F {
+		class TransportMagazines : TransportMagazines {
+			class _xx_30_Rnd_556x45_Mk318_Stanag {
+				count = 24;
+				magazine  = "30Rnd_556x45_Mk318_Stanag";
+			};
+			class _xx_30_Rnd_556x45_Mk262_Stanag {
+				count = 24;
+				magazine  = "30Rnd_556x45_Mk262_Stanag";
+			};
 		};
 	};
 };
