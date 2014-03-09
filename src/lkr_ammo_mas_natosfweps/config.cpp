fe23f5215278a169f9baad217eefc3a7cb18e705
Lukas Kropatschek <lukas@kropatschek.net>
2014-03-09 14:15:00 +0100

    updated massi weps config

--- src/lkr_ammo_mas_natosfweps/config.cpp
+++ src/lkr_ammo_mas_natosfweps/config.cpp
@@ -12,9 +12,12 @@ class CfgPatches {
 		requiredAddons[] = {
 			"lkr_556x45_NATO",
 			"lkr_762x51_NATO",
+			"lkr_545x39_Soviet",
+			"lkr_762x39_Soviet",
 			"lkr_9x19_Parabellum",
 			"mas_weapons",
-			"mas_weapons_m4"
+			"mas_weapons_m4",
+			"mas_weapons_ak"
 		};
 		author[] = {
 			"lukrop"
@@ -231,6 +234,162 @@ class CfgWeapons {
 			"20Rnd_mas_762x51_T_Stanag"
 		};
 	};
+
+	class arifle_Katiba_F;
+	class arifle_mas_ak_74m: arifle_Katiba_F {
+		magazines[] = {
+			"30Rnd_545x39_Mag",
+			"30Rnd_545x39_Mag_Tracer",
+			"30Rnd_545x39_7N10_Mag",
+			"30Rnd_545x39_7N22_Mag",
+			"100Rnd_545x39_Drum",
+			"100Rnd_545x39_Drum_Tracer",
+			"30Rnd_mas_545x39_mag",
+			"30Rnd_mas_545x39_T_mag"
+		};
+	};
+
+	class arifle_Katiba_GL_F;
+	class arifle_mas_ak_74m_gl: arifle_Katiba_GL_F {
+		magazines[] = {
+			"30Rnd_545x39_Mag",
+			"30Rnd_545x39_Mag_Tracer",
+			"30Rnd_545x39_7N10_Mag",
+			"30Rnd_545x39_7N22_Mag",
+			"100Rnd_545x39_Drum",
+			"100Rnd_545x39_Drum_Tracer",
+			"30Rnd_mas_545x39_mag",
+			"30Rnd_mas_545x39_T_mag"
+		};
+	};
+
+	class arifle_mas_akms: arifle_mas_ak_74m {
+		magazines[] = {
+			"30Rnd_762x39_Mag",
+			"30Rnd_762x39_Mag_Tracer",
+			"100Rnd_762x39_Drum",
+			"100Rnd_762x39_Drum_Tracer",
+			"30Rnd_mas_762x39_mag",
+			"30Rnd_mas_762x39_T_mag"
+		};
+	};
+
+	class arifle_mas_akms_gl: arifle_mas_ak_74m_gl {
+		magazines[] = {
+			"30Rnd_762x39_Mag",
+			"30Rnd_762x39_Mag_Tracer",
+			"100Rnd_762x39_Drum",
+			"100Rnd_762x39_Drum_Tracer",
+			"30Rnd_mas_762x39_mag",
+			"30Rnd_mas_762x39_T_mag"
+		};
+	};
+
+	class arifle_mas_akm_gl: arifle_mas_akms_gl {
+		magazines[] = {
+			"30Rnd_762x39_Mag",
+			"30Rnd_762x39_Mag_Tracer",
+			"100Rnd_762x39_Drum",
+			"100Rnd_762x39_Drum_Tracer",
+			"30Rnd_mas_762x39_mag",
+			"30Rnd_mas_762x39_T_mag"
+		};
+	};
+
+	class arifle_mas_m70_gl: arifle_mas_akm_gl {
+		magazines[] = {
+			"30Rnd_762x39_Mag",
+			"30Rnd_762x39_Mag_Tracer",
+			"100Rnd_762x39_Drum",
+			"100Rnd_762x39_Drum_Tracer",
+			"30Rnd_mas_762x39_mag",
+			"30Rnd_mas_762x39_T_mag"
+		};
+	};
+
+	class arifle_mas_m70ab_gl: arifle_mas_akms_gl {
+		magazines[] = {
+			"30Rnd_762x39_Mag",
+			"30Rnd_762x39_Mag_Tracer",
+			"100Rnd_762x39_Drum",
+			"100Rnd_762x39_Drum_Tracer",
+			"30Rnd_mas_762x39_mag",
+			"30Rnd_mas_762x39_T_mag"
+		};
+	};
+
+	class arifle_mas_aks74u: arifle_Katiba_F {
+		magazines[] = {
+			"30Rnd_545x39_Mag",
+			"30Rnd_545x39_Mag_Tracer",
+			"30Rnd_545x39_7N10_Mag",
+			"30Rnd_545x39_7N22_Mag",
+			"100Rnd_545x39_Drum",
+			"100Rnd_545x39_Drum_Tracer",
+			"30Rnd_mas_545x39_mag",
+			"30Rnd_mas_545x39_T_mag"
+		};
+	};
+
+	class srifle_mas_svd: srifle_EBR_F {
+		magazines[] = {
+			"10Rnd_762x54_Mag",
+			"10Rnd_762x54_Mag_Tracer",
+			"10Rnd_762x54_7N1_Mag",
+			"10Rnd_mas_762x54_mag",
+			"10Rnd_mas_762x54_T_mag"
+		};
+	};
+
+	class srifle_mas_m91: srifle_mas_svd {
+		magazines[] = {
+			"10Rnd_762x54_Mag",
+			"10Rnd_762x54_Mag_Tracer",
+			"10Rnd_762x54_7N1_Mag",
+			"10Rnd_mas_762x54_mag",
+			"10Rnd_mas_762x54_T_mag"
+		};
+	};
+
+	class LMG_mas_rpk_F: LMG_Mk200_F {
+		magazines[] = {
+			"100Rnd_545x39_Drum",
+			"100Rnd_545x39_Drum_Tracer",
+			"30Rnd_545x39_Mag",
+			"30Rnd_545x39_Mag_Tracer",
+			"30Rnd_545x39_7N10_Mag",
+			"30Rnd_545x39_7N22_Mag",
+			"100Rnd_mas_545x39_mag",
+			"100Rnd_mas_545x39_T_mag",
+			"30Rnd_mas_545x39_mag",
+			"30Rnd_mas_545x39_T_mag"
+		};
+	};
+
+	class LMG_mas_m72_F: LMG_mas_rpk_F {
+		magazines[] = {
+			"100Rnd_545x39_Drum",
+			"100Rnd_545x39_Drum_Tracer",
+			"30Rnd_545x39_Mag",
+			"30Rnd_545x39_Mag_Tracer",
+			"30Rnd_545x39_7N10_Mag",
+			"30Rnd_545x39_7N22_Mag",
+			"100Rnd_mas_545x39_mag",
+			"100Rnd_mas_545x39_T_mag",
+			"30Rnd_mas_545x39_mag",
+			"30Rnd_mas_545x39_T_mag"
+		};
+	};
+
+	class LMG_mas_pkm_F: LMG_Mk200_F {
+		magazines[] = {
+			"100Rnd_762x54_Box",
+			"100Rnd_762x54_Box_Tracer",
+			"100Rnd_mas_762x54_mag",
+			"100Rnd_mas_762x54_T_mag"
+		};
+	};
+
 };
 
 class CfgVehicles {
@@ -239,13 +398,12 @@ class CfgVehicles {
 		class TransportMagazines;
 	};
 	
-	/*
 	class EAST_Box_Base;
 	class Box_East_Wps_F : EAST_Box_Base
 	{
 		class TransportMagazines;
 	};
-	*/
+	
 	
 	class Box_mas_all_rifle_Wps_F : Box_NATO_Wps_F {
 		class TransportMagazines {
@@ -263,19 +421,74 @@ class CfgVehicles {
 				count=120;
 			};
 			
-			class _xx_30Rnd_mas_556x45_Mk318_Stanag_Tracer_Red {
-				magazine="30Rnd_556x45_Mk318_Stanag_Tracer_Red";
-				count=60;
-			};
-			
 			class _xx_30Rnd_556x45_Mk262_Stanag {
 				magazine="30Rnd_556x45_Mk262_Stanag";
 				count=120;
 			};
-			
-			class _xx_30Rnd_mas_556x45_Mk262_Stanag_Tracer_Red {
-				magazine="30Rnd_556x45_Mk262_Stanag_Tracer_Red";
-				count=60;
+
+			class _xx_30Rnd_762x39_Mag {
+				magazine = "30Rnd_762x39_Mag";
+				count = 60;
+			};
+
+			class _xx_30Rnd_762x39_Mag_Tracer {
+				magazine = "30Rnd_762x39_Mag_Tracer";
+				count = 60;
+			};
+
+			class _xx_30Rnd_545x39_Mag {
+				magazine = "30Rnd_545x39_Mag";
+				count = 60;
+			};
+
+			class _xx_30Rnd_545x39_Mag_Tracer {
+				magazine = "30Rnd_545x39_Mag_Tracer";
+				count = 60;
+			};
+
+			class _xx_30Rnd_545x39_7N10_Mag {
+				magazine = "30Rnd_545x39_7N10_Mag";
+				count = 60;
+			};
+
+			class _xx_30Rnd_545x39_7N22_Mag {
+				magazine = "30Rnd_545x39_7N22_Mag";
+				count = 60;
+			};
+
+			class _xx_100Rnd_545x39_Drum {
+				magazine = "100Rnd_545x39_Drum";
+				count = 60;
+			};
+
+			class _xx_100Rnd_545x39_Drum_Tracer {
+				magazine = "100Rnd_545x39_Drum_Tracer";
+				count = 60;
+			};
+
+			class _xx_10Rnd_762x54_Mag {
+				magazine = "10Rnd_762x54_Mag";
+				count = 60;
+			};
+
+			class _xx_10Rnd_762x54_Mag_Tracer {
+				magazine = "10Rnd_762x54_Mag_Tracer";
+				count = 60;
+			};
+
+			class _xx_10Rnd_762x54_7N1_Mag {
+				magazine = "10Rnd_762x54_7N1_Mag";
+				count = 60;
+			};
+
+			class _xx_100Rnd_762x54_Box {
+				magazine = "100Rnd_762x54_Box";
+				count = 60;
+			};
+
+			class _xx_100Rnd_762x54_Box_Tracer {
+				magazine = "100Rnd_762x54_Box_Tracer";
+				count = 60;
 			};
 			
 		};
@@ -298,27 +511,85 @@ class CfgVehicles {
 				count=120;
 			};
 			
-			class _xx_30Rnd_mas_556x45_Mk318_Stanag_Tracer_Red {
-				magazine="30Rnd_556x45_Mk318_Stanag_Tracer_Red";
-				count=60;
-			};
 			
 			class _xx_30Rnd_556x45_Mk262_Stanag {
 				magazine="30Rnd_556x45_Mk262_Stanag";
 				count=120;
 			};
 			
-			class _xx_30Rnd_mas_556x45_Mk262_Stanag_Tracer_Red {
-				magazine="30Rnd_556x45_Mk262_Stanag_Tracer_Red";
-				count=60;
-			};
-			
+
 		};
 	};
 	
-	/*
+	
 	class Box_mas_ru_rifle_Wps_F: Box_East_Wps_F
 	{
+		class TransportMagazines {
+			class _xx_30Rnd_762x39_Mag {
+				magazine = "30Rnd_762x39_Mag";
+				count = 60;
+			};
+
+			class _xx_30Rnd_762x39_Mag_Tracer {
+				magazine = "30Rnd_762x39_Mag_Tracer";
+				count = 60;
+			};
+
+			class _xx_30Rnd_545x39_Mag {
+				magazine = "30Rnd_545x39_Mag";
+				count = 60;
+			};
+
+			class _xx_30Rnd_545x39_Mag_Tracer {
+				magazine = "30Rnd_545x39_Mag_Tracer";
+				count = 60;
+			};
+
+			class _xx_30Rnd_545x39_7N10_Mag {
+				magazine = "30Rnd_545x39_7N10_Mag";
+				count = 60;
+			};
+
+			class _xx_30Rnd_545x39_7N22_Mag {
+				magazine = "30Rnd_545x39_7N22_Mag";
+				count = 60;
+			};
+
+			class _xx_100Rnd_545x39_Drum {
+				magazine = "100Rnd_545x39_Drum";
+				count = 60;
+			};
+
+			class _xx_100Rnd_545x39_Drum_Tracer {
+				magazine = "100Rnd_545x39_Drum_Tracer";
+				count = 60;
+			};
+
+			class _xx_10Rnd_762x54_Mag {
+				magazine = "10Rnd_762x54_Mag";
+				count = 60;
+			};
+
+			class _xx_10Rnd_762x54_Mag_Tracer {
+				magazine = "10Rnd_762x54_Mag_Tracer";
+				count = 60;
+			};
+
+			class _xx_10Rnd_762x54_7N1_Mag {
+				magazine = "10Rnd_762x54_7N1_Mag";
+				count = 60;
+			};
+
+			class _xx_100Rnd_762x54_Box {
+				magazine = "100Rnd_762x54_Box";
+				count = 60;
+			};
+
+			class _xx_100Rnd_762x54_Box_Tracer {
+				magazine = "100Rnd_762x54_Box_Tracer";
+				count = 60;
+			};
+		};
 	};
-	*/
+	
 };
